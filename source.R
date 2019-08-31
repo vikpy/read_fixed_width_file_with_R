e6dfd1430252877e8b20f4ec92eabf4ecf2c6084
vikpy <vikraant.py@gmail.com>
2019-08-31 23:47:15 +0530

    Completed the code as per the instructions

--- source.R
+++ source.R
@@ -1,31 +1,38 @@
 path_for_data <-  "./data.txt"
+setwd("C:/Users/Vikraant Pai/workspace/read_fixed_width_file_with_R")
 
 #fetching the values from the fwf dataset, skipping the column headers and mentioning the size using the column sizes 
-df_weather_data <- read.fwf(path_for_data, c(6, 5, 3, 3, 4, 3, 5, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3), header = F, skip=1 )
+df_weather_data <- read.fwf(path_for_data, c(6, 5, 3, 3, 4, 3, 5, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3), header = F, skip=33 , n=2455)
 
 #Adding the column names using the dataset
-colnames(df_weather_data) <- c("INDEX", "YEAR",  "MN",  "HR",  ".RH",  "NO",  ".MWS",  "W1",  "W2", "W3",  "W4",  "NO",  ".N", "NE",  ".E",  "SE",  ".S",  "SW",  ".W", "NW",  "CA",  "VA",  "NO")
+colnames(df_weather_data) <- c("INDEX", "YEAR",  "MN",  "HR",  "RH",  "NO1",  "MWS",  "W1",  "W2", "W3",  "W4",  "NO2",  "N", "NE",  "E",  "SE",  "S",  "SW",  "W", "NW",  "CA",  "VA",  "NO3")
+#We will refer to column names starting from RH
+col_names_list <- c("RH",  "NO1",  "MWS",  "W1",  "W2", "W3",  "W4",  "NO2",  "N", "NE",  "E",  "SE",  "S",  "SW",  "W", "NW",  "CA",  "VA",  "NO3")
 
 #Splitting the data into 830 dataset and 1730 dataset 
-df_weather_data_830  <- df_weather_data[df_weather_data$HR == 3, ]
+df_weather_data_830  <- df_weather_data[ df_weather_data$HR == 3, ]
+df_weather_data_830
 summary(df_weather_data_830)
 df_weather_data_1730 <- df_weather_data[df_weather_data$HR == 12,]
+df_weather_data_1730
 summary(df_weather_data_1730)
 
-#Defining fucntion to replace mean
-replaceNAwithMean  <-  function(x) replace(x, is.na(x), mean(x, na.rm = TRUE))
+#Finding means for 830 data set and replacing them with means
+df_weather_data_830_means <-  aggregate( df_weather_data_830[, 5:23] , by = list(df_weather_data_830$MN), FUN=function(x) return(mean(x, na.rm = T)) )
+df_weather_data_830_means
+for(column_name in col_names_list){
+  for(month_number in c(1:12) ){
+    print( df_weather_data_830[which( is.na(df_weather_data_830[column_name]) & df_weather_data_830$MN == month_number) ,][column_name] ) <- df_weather_data_830_means[column_name][month_number, ]
+  }
+}
 
-#Replacing Misssing data with mean 
-lapply(df_weather_data_830, replaceNAwithMean)
-summary(df_weather_data_830)
-lapply(df_weather_data_1730, replaceNAwithMean)
-summary(df_weather_data_830)
+df_weather_data_830
 
 
 #Checking if the count of line items are same in all the three data sets
 if( length(df_weather_data_1730[,1]) + length(df_weather_data_830[,1]) ==  length(df_weather_data[,1]) ) {
 
- sprintf("Num of line items are same")
+  sprintf("Num of line items are same")
   
 } else {
   
@@ -36,4 +43,3 @@ if( length(df_weather_data_1730[,1]) + length(df_weather_data_830[,1]) ==  lengt
 #Writing the data in csv
 write.csv(df_weather_data_1730, file = "./weather_data_1730.csv")
 write.csv(df_weather_data_830, file = "./weather_data_830.csv")
-
